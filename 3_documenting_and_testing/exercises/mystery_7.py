61ad99e789879944b2511afcd6b7694bc26d9ebd
Anik Kumar Adhikary <anikkumaradhikary@gmail.com>
2024-12-16 21:22:57 +0530

    mystery_7 update

--- 3_documenting_and_testing/exercises/mystery_7.py
+++ 3_documenting_and_testing/exercises/mystery_7.py
@@ -1,6 +1,21 @@
 def mystery_7(a, b):
+    """The function filters a sequence of elements by checking if each element contains the value`b`
+    
+    Parameters:
+    - a(iterable): a sequence of elements to be checked
+    - b(any): the value to check for within each element of `a`
+
+    Returns:
+    A new list containing elements from `a` that contains `b`
+
+    Example:
+    >>> mystery_7(['Homnelander', 'Soldierboy', 'Butcher'], 'o')
+    ['Homelander','Butcher']
+    """
+    #`c` is initialised to store the new list
     c = []
     for d in a:
         if b in d:
+            # all elements appends to a new list that is `c`
             c.append(d)
     return c
--- 3_documenting_and_testing/exercises/tests/test_mystery_7.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_7.py
@@ -3,7 +3,7 @@ import unittest
 from ..mystery_7 import mystery_7
 
 class TestMystery7(unittest.TestCase):
-    """ """
+    """Test the mystery_7 function"""
 
     def test_minimal_input_list(self):
         """"""
@@ -12,3 +12,7 @@ class TestMystery7(unittest.TestCase):
     def test_minimal_input_string(self):
         """"""
         self.assertEqual(mystery_7('', ''), [])
+
+    def test_Supes_input_string(self):
+        """"""
+        self.assertEqual(mystery_7(['Homnelander', 'Soldierboy', 'Butcher'], 'o'), ['Homelander','Butcher'])
