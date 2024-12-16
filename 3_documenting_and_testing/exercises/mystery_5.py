2ef427f1ad3b26ac189749c88810d4c4f4f26f38
Anik Kumar Adhikary <anikkumaradhikary@gmail.com>
2024-12-16 20:50:37 +0530

    mystery_5 update

--- 3_documenting_and_testing/exercises/mystery_5.py
+++ 3_documenting_and_testing/exercises/mystery_5.py
@@ -1,6 +1,23 @@
-def mystery_5(a, b):
+def mystery_5(a, b=None):
+    """This function sorts a lists of strings or numbers
+    - Strings are sorted by UNICODE value
+    - Numbers are sorted in ascending order
+
+    Examples:
+    >>> mystery_5(['n','i','k','e'],[])
+    ['e','i','k','n']
+
+    >>> mystery_5([3,1,4,1,5,9,2,6],[])
+    [1,1,2,3,4,5,6,9]
+    """
+    # makes sure there is a list to  fill
+    if b is None:
+    # moves items from one list to another in sorted order
+        b = []
     while a:
+        # finds the lowest item in the first list
         c = min(a)
+        # moves it to the second list
         a.remove(c)
         b.append(c)
     return b
--- 3_documenting_and_testing/exercises/tests/test_mystery_5.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_5.py
@@ -3,9 +3,19 @@ import unittest
 from ..mystery_5 import mystery_5
 
 class TestMystery5(unittest.TestCase):
-    """ """
+    """Test the mystery_5 function"""
 
-    def test_minimal_input(self):
+    def test_letters_input(self):
         """"""
-        self.assertEqual(mystery_5([], []), [])
+        self.assertEqual(mystery_5(['n','i','k','e'], []), ['e','i','k','n'])
+
+    def test_letters_input(self):
+        """"""
+        self.assertEqual(mystery_5(['king','ace','queen','joker'], []), ['ace','joker','king','queen'])
         
+    def test_letters_input(self):
+        """"""
+        self.assertEqual(mystery_5([3,1,4,1,5,9,2,6], []), [1,1,2,3,4,5,6,9])
+        
+
+   
