77192b6e14a0cf7930eeafc56453b0051cd38736
Anik Kumar Adhikary <anikkumaradhikary@gmail.com>
2024-12-16 19:52:57 +0530

    mystery_2 update

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,5 +1,15 @@
 def mystery_2(a):
+    """It returns the length of the input list, either no. or string
+     Parameters:
+     - a: no.(int/float) or string whose length is to be found
+
+     Example:
+     >>> mystery_2('Homelander')
+    [10]
+    """
+
     if len(a) == 0:
         return None
-
+    # returns length of the input if it is not empty
+    # returns None if the input is empty
     return len(a)
--- 3_documenting_and_testing/exercises/tests/test_mystery_2.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_2.py
@@ -3,12 +3,28 @@ import unittest
 from ..mystery_2 import mystery_2
 
 class TestMystery2(unittest.TestCase):
-    """ """
+    """Test the mystery_2 function """
 
     def test_minimal_list_input(self):
-        """"""
+        """It should return None"""
         self.assertEqual(mystery_2([]), None)
 
     def test_minimal_string_input(self):
-        """"""
+        """It should return None"""
         self.assertEqual(mystery_2(''), None)
+
+    def test_sorted_number_input(self):
+        """It should return 5"""
+        self.assertEqual(mystery_2([1,2,3,4,5]), 5)
+
+    def test_unsorted_numbers_input(self):
+        """It should return 6"""
+        self.assertEqual(mystery_2([5,9,8,6,1,4]), 6)
+
+    def test_unsorted_numbers_input(self):
+        """It should return 3"""
+        self.assertEqual(mystery_2([5.9,8.6,1.4]), 3)
+
+    def test_minimal_string_input(self):
+        """It should return 10"""
+        self.assertEqual(mystery_2('Homelander'), 10)
