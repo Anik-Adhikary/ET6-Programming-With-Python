f48921d6013f0579840a090a7963736e8c92f633
Anik Kumar Adhikary <anikkumaradhikary@gmail.com>
2024-12-16 21:03:59 +0530

    mystery_6 update

--- 3_documenting_and_testing/exercises/mystery_6.py
+++ 3_documenting_and_testing/exercises/mystery_6.py
@@ -1,4 +1,19 @@
 def mystery_6(a, b):
+    """ It generates a list of length `a`, whwere the list starts with `b` and
+    thereafter increases the value of `b` by 1 for each subsequent element.
+
+    Parameters:
+    - a(int): The number of elements in the output list
+    - b(int): The starting value of the lsit
+
+    Returns:
+    A list containing `a` elements, starting from `b` and incrementig by 1 for each element
+
+    Examples:
+    >>> mystery_6(3, 3) 
+    [3,4,5]
+    
+    """
     if a == 0:
         return []
 
--- 3_documenting_and_testing/exercises/tests/test_mystery_6.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_6.py
@@ -3,8 +3,16 @@ import unittest
 from ..mystery_6 import mystery_6
 
 class TestMystery6(unittest.TestCase):
-    """ """
+    """Test the mystery_6 function"""
 
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_6(0, 0), [])
+
+    def test_minimal_input(self):
+        """"""
+        self.assertEqual(mystery_6(3, 3), [3,4,5])
+
+    def test_minimal_input(self):
+        """"""
+        self.assertEqual(mystery_6(9, 5), [5,6,7,8,9,10,11,12,13])
