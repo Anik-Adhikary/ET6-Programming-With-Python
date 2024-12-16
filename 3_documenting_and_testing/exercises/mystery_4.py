d7712ef19f3a1b71843a9425db02113e4e57e3ff
Anik Kumar Adhikary <anikkumaradhikary@gmail.com>
2024-12-16 20:33:07 +0530

    mystery_4 update

--- 3_documenting_and_testing/exercises/mystery_4.py
+++ 3_documenting_and_testing/exercises/mystery_4.py
@@ -1,8 +1,23 @@
 def mystery_4(a):
+    """Generates a lsit of sorted positive integers from 0 to a-1
+
+    Parameters:
+    - a(int): is the length of the lists to be generated, must be a non-negative integer.
+
+    Returns:
+    - A list containing positive integers from 0 to a-1
+
+    Example:
+    >>> mystery_4(8)
+    [0,1,2,3,4,5,6,7]
+    """
+    # initialises an empty list
     b = []
 
+    #  is initialised to 0 to track the next number to append
     c = 0
     while len(b) < a:
+        # keeps appending `c`to `b` and increments `c` by 1 until the length of `b` equals `a`
         b.append(c)
         c = c + 1
 
--- 3_documenting_and_testing/exercises/tests/test_mystery_4.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_4.py
@@ -3,8 +3,12 @@ import unittest
 from ..mystery_4 import mystery_4
 
 class TestMystery4(unittest.TestCase):
-    """ """
+    """Test the mystery_4 function"""
 
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_4(0), [])
+    
+    def test_minimal_input(self):
+        """It should return 0,1,2,3,4,5,6,7"""
+        self.assertEqual(mystery_4(8), [0,1,2,3,4,5,6,7])
