2394ca258be6060a8957889e8849561ca27f9eef
Anik Kumar Adhikary <anikkumaradhikary@gmail.com>
2024-12-16 20:10:52 +0530

    mystery_3 update

--- 3_documenting_and_testing/exercises/mystery_3.py
+++ 3_documenting_and_testing/exercises/mystery_3.py
@@ -1,7 +1,24 @@
 def mystery_3(a, b):
+    """It compares two numbers and returns a value based on some conditions
+    
+        Parameters
+        - a: int/float: first no.
+        - b: int/float: second no.
+
+        Examples:
+        >>> mystery_3(3,9)
+        3
+        >>> mystery_3(9,3)
+        3
+        >>> mystery_3(9,9)
+        18
+    """
+    # if a smaller than b, then a is returned
     if a < b:
         return a
+    # if b smaller than a, then b is returned
     elif a > b:
         return b
+    # if a equals b, then their sum is returned
     else:
         return a + b
--- 3_documenting_and_testing/exercises/tests/test_mystery_3.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_3.py
@@ -3,8 +3,28 @@ import unittest
 from ..mystery_3 import mystery_3
 
 class TestMystery3(unittest.TestCase):
-    """ """
+    """Test the mystery_3 function"""
 
     def test_minimal_input(self):
-        """"""
+        """It should return 0"""
         self.assertEqual(mystery_3(0, 0), 0)
+    
+    def test_a_smaller_than_b_input(self):
+        """It should return 3"""
+        self.assertEqual(mystery_3(3, 9), 3)
+
+    def test_b_smaller_than_a_input(self):
+        """It should return 3"""
+        self.assertEqual(mystery_3(9, 3), 3)
+
+    def test_a_equals_b_input(self):
+        """It should return 18"""
+        self.assertEqual(mystery_3(9, 9), 18)
+
+    def test_float_input(self):
+        """It should return 9.1"""
+        self.assertEqual(mystery_3(9.1, 9.2), 9.1)
+
+    def test_float_and_integer_input(self):
+        """It should return 2"""
+        self.assertEqual(mystery_3(3.4, 2), 2)
