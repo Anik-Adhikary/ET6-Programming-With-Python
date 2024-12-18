d905b3d8131c05c389c85ac12aa0cd01ae1b50ce
Anik Kumar Adhikary <anikkumaradhikary@gmail.com>
2024-12-18 16:46:26 +0530

    newly updated

--- 5_tdd_with_llms/exercises/sum_evens_and_odds.py
+++ 5_tdd_with_llms/exercises/sum_evens_and_odds.py
@@ -4,3 +4,26 @@ Write a function that takes a list of numbers
 and returns a dictionary with sums of the even and odd numbers in the list.
 
 """
+def sum_even_odd(numbers: list) -> dict:
+    """This function takes a list of numbers and returns a dictionary with 
+    sums of the even and odd numbers in the list.
+    
+    Parameters:
+    - numbers (list): A list of integers to test the function with.
+        
+    Returns:
+    - the result is verified using self.assertEqual
+    """
+    
+    result = {'even': 0, 'odd': 0}
+    
+    for num in numbers:
+        # for even numbers
+        if num % 2 == 0:
+            result['even'] += num
+
+        # for odd numbers 
+        else:
+            result['odd'] += num
+
+    return result
--- 5_tdd_with_llms/exercises/tests/test_sum_evens_and_odds.py
+++ 5_tdd_with_llms/exercises/tests/test_sum_evens_and_odds.py
@@ -1,1 +1,25 @@
+import unittest
 
+from ..sum_evens_and_odds import sum_even_odd
+class test_sum_evens_and_odds(unittest.TestCase):
+  """To test the function sum_even_odd"""
+  
+  def test_sorted_odd_and_even_numbers(self):
+        """Taking a sorted list of numbers, expected result-> even: 30, odd: 25"""
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        expected_result = {'even': 30, 'odd': 25}
+       
+        result = sum_even_odd(numbers)
+        self.assertEqual(result, expected_result) # Check if the result matches the expected
+
+  def test_unsorted_odd_and_even_numbers(self):
+        """Taking an unsorted list of numbers, expected result-> even: 62, odd: 46"""
+        numbers = [11,9,1,14,8,13,16,5,24,7]
+        expected_result = {'even': 62, 'odd': 46}
+       
+        result = sum_even_odd(numbers)
+        self.assertEqual(result, expected_result) # Check if the result matches the expected
+
+
+
+  
